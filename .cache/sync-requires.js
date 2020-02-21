2fbc1d7b64d1823454fb63aabb20913cca39daf8
Phil <phillip.mckenzie@colorado.edu>
2020-02-21 13:14:11 -0700

    clearing caches

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-work-js": hot(preferDefault(require("/Users/philmckenzie 1/Desktop/philmckenziedigital/src/templates/work.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/philmckenzie 1/Desktop/philmckenziedigital/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/philmckenzie 1/Desktop/philmckenziedigital/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/philmckenzie 1/Desktop/philmckenziedigital/src/pages/about.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/philmckenzie 1/Desktop/philmckenziedigital/src/pages/index.js")))
